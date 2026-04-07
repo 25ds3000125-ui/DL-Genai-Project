04c076b2434ecdf3bf72a31394740e992f06fad0
25ds3000125-ui <25ds3000125@ds.study.iitm.ac.in>
2026-04-08 00:16:47 +0530

    Refactor config.py for model settings and genre mapping
    Refactor configuration settings and hyperparameters for CNN and AST models. Introduce label mapping for genres.

--- src/config.py
+++ src/config.py
@@ -1,17 +1,33 @@
-GENRES = ["blues","classical","country","disco","hiphop",
-          "jazz","metal","pop","reggae","rock"]
+#!/usr/bin/env python
+# coding: utf-8
 
-SR = 16000
-DURATION = 10
-SAMPLES = SR * DURATION
+import torch
 
-EPOCHS = 5
-LR = 2e-5
-BATCH_SIZE = 8
+# Configuration & Hyperparameters
+# Defines sampling rates, durations, and training settings for CNN and AST models
+class CFG:
+    SR = 22050
+    AST_SR = 16000
+    DURATION = 5
+    AST_DURATION = 10
+    SAMPLES = SR * DURATION
+    AST_SAMPLES = AST_SR * AST_DURATION
+    N_MELS = 128
+    MEL_WIDTH = 216
+    BATCH_SIZE = 16
+    AST_BATCH = 8
+    EPOCHS = 4
+    AST_EPOCHS = 8
+    LR = 1e-3
+    AST_LR = 2e-5
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 BASE_PATH = "/kaggle/input/competitions/jan-2026-dl-gen-ai-project/messy_mashup"
-
 TRAIN_PATH = f"{BASE_PATH}/genres_stems"
 NOISE_PATH = f"{BASE_PATH}/ESC-50-master/audio"
 TEST_PATH = f"{BASE_PATH}/mashups"
 TEST_CSV = f"{BASE_PATH}/test.csv"
+
+GENRES = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
+label2idx = {g:i for i,g in enumerate(GENRES)}
+idx2label = {i:g for g,i in label2idx.items()}
